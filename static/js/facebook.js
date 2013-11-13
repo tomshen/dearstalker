940ea352c73c1765790fc422f218b3b6f70f4f7f
Tom Shen <tom@tomshen.me>
2013-11-13 18:52:41 -0500

    Add getting of friends' profile pictures

--- static/js/facebook.js
+++ static/js/facebook.js
@@ -8,14 +8,18 @@ var Facebook = function () {
     }
   }
 
-  function get_current_user_profile(callback) {
-    FB.api('/me?access_token=' + getAccessToken(), function (user) {
+  function get_current_user_profile(callback, fields) {
+    if (typeof fields === 'undefined')
+      fields = ['name', 'picture'];
+    FB.api('/me?fields=' + fields.join() + '&access_token=' + getAccessToken(), function (user) {
       callback(user);
     });
   }
 
-  function get_profile(user_id, callback) {
-    FB.api('/?' + user_id + '&access_token=' + getAccessToken(), function (user) {
+  function get_profile(user_id, callback, fields) {
+    if (typeof fields === 'undefined')
+      fields = ['name', 'picture'];
+    FB.api('/' + user_id + '?fields=' + fields.join() + '&access_token=' + getAccessToken(), function (user) {
       callback(user);
     });
   }
